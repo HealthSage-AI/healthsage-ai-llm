d905afd4eca629603077a6eac554c087cd090f42
datadoktergroen <96254933+datadoktergroen@users.noreply.github.com>
2024-02-13 14:15:08 +0100

    cleanup redundant imports

--- src/healthsageai/note_to_fhir/inference/__init__.py
+++ src/healthsageai/note_to_fhir/inference/__init__.py
@@ -1,1 +1,1 @@
-from src.note_to_fhir.inference.note_to_fhir import NoteToFhir13b, NoteToFhir8x7b  # noqa: F401
+from healthsageai.note_to_fhir.inference.note_to_fhir import NoteToFhir13b, NoteToFhir8x7b  # noqa: F401
